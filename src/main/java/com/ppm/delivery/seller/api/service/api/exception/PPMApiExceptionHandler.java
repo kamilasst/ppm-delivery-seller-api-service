c3e79cb1e30d6ec995e2ddf438e5c900808a53d1
kamilasst <kamilasantosdev@gmail.com>
2025-02-20 14:06:14 -0300

    [000102-10] feat: validate supported countries in Interceptor using application.yaml

--- src/main/java/com/ppm/delivery/seller/api/service/api/exception/PPMApiExceptionHandler.java
+++ src/main/java/com/ppm/delivery/seller/api/service/api/exception/PPMApiExceptionHandler.java
@@ -1,12 +1,15 @@
 package com.ppm.delivery.seller.api.service.api.exception;
 
-import com.ppm.delivery.seller.api.service.exception.PPMBadRequestException;
+import com.ppm.delivery.seller.api.service.exception.CountryNotSupportedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class PPMApiExceptionHandler {
@@ -23,10 +26,12 @@ public class PPMApiExceptionHandler {
         return new ResponseEntity<>(errors.toString(), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(PPMBadRequestException.class)
-    public ResponseEntity<String> handlePPMBadRequestException(PPMBadRequestException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(CountryNotSupportedException.class)
+    public ResponseEntity<Map<String, String>> handleCountryNotSupportedException(CountryNotSupportedException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", ex.getMessage());
 
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
 }
--- src/main/java/com/ppm/delivery/seller/api/service/api/interceptor/RequestValidator.java
+++ src/main/java/com/ppm/delivery/seller/api/service/api/interceptor/RequestValidator.java
@@ -1,19 +1,33 @@
 package com.ppm.delivery.seller.api.service.api.interceptor;
 
-import com.ppm.delivery.seller.api.service.exception.MessageErrorConstants;
-import com.ppm.delivery.seller.api.service.exception.PPMBadRequestException;
+import com.ppm.delivery.seller.api.service.api.config.SellerConfig;
 import com.ppm.delivery.seller.api.service.api.domain.request.header.Header;
+import com.ppm.delivery.seller.api.service.exception.CountryNotSupportedException;
+import com.ppm.delivery.seller.api.service.exception.MessageErrorConstants;
 import io.micrometer.common.util.StringUtils;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class RequestValidator {
+
+    private SellerConfig sellerConfig;
+
+    public RequestValidator(SellerConfig sellerConfig){
+        this.sellerConfig = sellerConfig;
+    }
 
     public boolean validate(final Header header){
 
         final String country = header.metadata().country();
         if (StringUtils.isBlank(country)) {
-            throw new PPMBadRequestException(MessageErrorConstants.ERROR_COUNTRY_REQUIRED_HEADER);
+            throw new CountryNotSupportedException(MessageErrorConstants.ERROR_COUNTRY_REQUIRED_HEADER);
+        }
+
+        List<String> supportedCountries = sellerConfig.getSupportedCountries();
+        if (supportedCountries == null || !supportedCountries.contains(country)) {
+            throw new CountryNotSupportedException(String.format(MessageErrorConstants.ERROR_COUNTRY_NOT_SUPPORTED, country));
         }
         return true;
     }
