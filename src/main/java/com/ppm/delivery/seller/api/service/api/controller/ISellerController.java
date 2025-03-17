6011899410c8721a00e2620555b20fa00a98a635
kamilasst <kamilasantosdev@gmail.com>
2025-03-17 18:04:16 -0300

    Create PATCH endpoint in Controller and Interface to test seller update

--- src/main/java/com/ppm/delivery/seller/api/service/api/controller/ISellerController.java
+++ src/main/java/com/ppm/delivery/seller/api/service/api/controller/ISellerController.java
@@ -4,12 +4,19 @@ import com.ppm.delivery.seller.api.service.api.domain.request.SellerDTORequest;
 import com.ppm.delivery.seller.api.service.api.domain.response.SellerDTOResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
 
 public interface ISellerController {
 
     @PostMapping("/create")
     ResponseEntity<SellerDTOResponse> create(@Valid @RequestBody SellerDTORequest code);
+
+    @PatchMapping("/{code}")
+    ResponseEntity<Map<String, Object>> patchV1(@Valid @PathVariable String code, @RequestBody Map<String, Object> patchSeller);
 
 }
--- src/main/java/com/ppm/delivery/seller/api/service/api/controller/SellerController.java
+++ src/main/java/com/ppm/delivery/seller/api/service/api/controller/SellerController.java
@@ -6,7 +6,12 @@ import com.ppm.delivery.seller.api.service.service.SellerService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/seller")
@@ -22,6 +27,12 @@ public class SellerController implements ISellerController{
     public ResponseEntity<SellerDTOResponse> create(@Valid @RequestBody SellerDTORequest sellerDTORequest){
         SellerDTOResponse sellerDTOResponse = sellerService.create(sellerDTORequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(sellerDTOResponse);
+    }
+
+    @Override
+    public ResponseEntity<Map<String, Object>> patchV1(@PathVariable String code,
+                                                       @RequestBody Map<String, Object> patchSeller) {
+        return ResponseEntity.ok(patchSeller);
     }
 
 }
