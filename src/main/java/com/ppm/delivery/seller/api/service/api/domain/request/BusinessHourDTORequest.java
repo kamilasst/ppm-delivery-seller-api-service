765b77f47d634c37954f5dbfcb8a372d2caa22c5
kamilasst <kamilasantosdev@gmail.com>
2025-04-29 19:51:56 -0300

    [000106-10] feat: Implemented validation for 'HH:mm:ss' time format and 24-hour range. Created unit and integration tests.

--- src/main/java/com/ppm/delivery/seller/api/service/api/domain/request/BusinessHourDTORequest.java
+++ src/main/java/com/ppm/delivery/seller/api/service/api/domain/request/BusinessHourDTORequest.java
@@ -1,14 +1,22 @@
 package com.ppm.delivery.seller.api.service.api.domain.request;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 
 @Builder
 public record BusinessHourDTORequest(
+
         String dayOfWeek,
-        @Pattern(regexp = "^([01]?[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$", message = "Open time must be in HH:mm:ss format")
+        @Pattern(
+                regexp = "^([01]?[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$",
+                message = "Invalid opening time. Times must be in 24-hour format, i.e., HH:mm:ss (e.g., 08:00:00 or 23:59:00)."
+        )
         String openAt,
-        @Pattern(regexp = "^([01]?[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$", message = "Open time must be in HH:mm:ss format")
+        @Pattern(
+                regexp = "^([01]?[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$",
+                message = "Invalid closing time. Times must be in 24-hour format, i.e., HH:mm:ss (e.g., 08:00:00 or 23:59:00)."
+        )
         String closeAt
 
 ) {
