14da2ee25117f0920facce2c27b0d82c37007e61
Kamila Santos <kamilasantosdev@gmail.com>
2025-06-10 15:15:54 -0300

    [000108-01] feat:add method to find active sellers nearby using Haversine formula

--- src/main/java/com/ppm/delivery/seller/api/service/repository/ISellerRepository.java
+++ src/main/java/com/ppm/delivery/seller/api/service/repository/ISellerRepository.java
@@ -2,8 +2,11 @@ package com.ppm.delivery.seller.api.service.repository;
 
 import com.ppm.delivery.seller.api.service.domain.model.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,5 +14,22 @@ public interface ISellerRepository extends JpaRepository<Seller, String> {
 
     boolean existsByIdentificationCode(String identificationCode);
     Optional<Seller> findByCode(String code);
+
+    @Query(value = """
+   SELECT * FROM seller s
+   WHERE s.status = 'ACTIVE'
+   AND (
+       6371000 * acos(
+           cos(radians(:lat)) * cos(radians(s.location_latitude)) *
+           cos(radians(s.location_longitude) - radians(:lng)) +
+           sin(radians(:lat)) * sin(radians(s.location_latitude))
+       )
+   ) <= :radius
+""", nativeQuery = true)
+    List<Seller> findActiveSellersNear(
+            @Param("lat") double latitude,
+            @Param("lng") double longitude,
+            @Param("radius") double radiusInMeters
+    );
 
 }
