e081d436f2d9d20fb031e05e617ca0bdbe5873f9
Kamila Santos <kamilasantosdev@gmail.com>
2025-06-16 18:09:57 -0300

    [000108-03] feat: create GET /api/seller/available endpoint in controller to fetch available sellers via sellerService

--- src/main/java/com/ppm/delivery/seller/api/service/api/controller/ISellerController.java
+++ src/main/java/com/ppm/delivery/seller/api/service/api/controller/ISellerController.java
@@ -1,15 +1,19 @@
 package com.ppm.delivery.seller.api.service.api.controller;
 
 import com.ppm.delivery.seller.api.service.api.domain.request.SellerDTORequest;
+import com.ppm.delivery.seller.api.service.api.domain.request.SellerNearSearchRequest;
 import com.ppm.delivery.seller.api.service.api.domain.request.SellerUpdateDTORequest;
 import com.ppm.delivery.seller.api.service.api.domain.response.SellerDTOResponse;
 import com.ppm.delivery.seller.api.service.api.domain.response.SellerUpdateDTOResponse;
+import com.ppm.delivery.seller.api.service.domain.model.Seller;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 public interface ISellerController {
 
@@ -18,5 +22,8 @@ public interface ISellerController {
 
     @PatchMapping("/patch/{code}")
     ResponseEntity<SellerUpdateDTOResponse> patchV1(@Valid @PathVariable String code, @RequestBody SellerUpdateDTORequest sellerUpdateDTO);
+
+    @PostMapping("/available")
+    ResponseEntity<List<Seller>> getAvailableSellers(@Valid @RequestBody SellerNearSearchRequest request);
 
 }
--- src/main/java/com/ppm/delivery/seller/api/service/api/controller/SellerController.java
+++ src/main/java/com/ppm/delivery/seller/api/service/api/controller/SellerController.java
@@ -1,17 +1,18 @@
 package com.ppm.delivery.seller.api.service.api.controller;
 
 import com.ppm.delivery.seller.api.service.api.domain.request.SellerDTORequest;
+import com.ppm.delivery.seller.api.service.api.domain.request.SellerNearSearchRequest;
 import com.ppm.delivery.seller.api.service.api.domain.request.SellerUpdateDTORequest;
 import com.ppm.delivery.seller.api.service.api.domain.response.SellerDTOResponse;
 import com.ppm.delivery.seller.api.service.api.domain.response.SellerUpdateDTOResponse;
+import com.ppm.delivery.seller.api.service.domain.model.Seller;
 import com.ppm.delivery.seller.api.service.service.ISellerService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/seller")
@@ -33,6 +34,17 @@ public class SellerController implements ISellerController{
     public ResponseEntity<SellerUpdateDTOResponse> patchV1(@PathVariable String code, @Valid @RequestBody SellerUpdateDTORequest sellerUpdateDTO) {
         SellerUpdateDTOResponse response = sellerService.update(code, sellerUpdateDTO);
         return ResponseEntity.ok(response);
+    }
+
+    @Override
+    public ResponseEntity<List<Seller>> getAvailableSellers(SellerNearSearchRequest request) {
+        List<Seller> sellers = sellerService.getAvailableSellers(request);
+
+
+        if (sellers.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(sellers);
     }
 
 }
